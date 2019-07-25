f34ef2ff8ae45a24c8f236b976d83d495ac4dcc5
Brent <brent.batch@hotmail.com>
2019-07-25 17:56:28 +0200

    Update physics.lua

--- Scripts/SE_Libs/physics.lua
+++ Scripts/SE_Libs/physics.lua
@@ -6,19 +6,19 @@ sm.__SE_Version._Physics = version
 print("Loading Physics library")
 
 -- set local gravity:
-if not sm.localGravityControllers then sm.localGravityController = {} end
+local localGravityControllers = {}
 function sm.physics.setLocalGravity(self, gravity, shape, dir, drag) -- an option: scriptclass has to perform this function every tick to work instead of janky overwrite
 	dir = (dir or sm.vec3.new(0,0,1)):normalize()
 	drag = (drag ~= nil and drag or 1)
 	shape = shape or self.shape
-	if dir == sm.vec3.new(0,0,1) and gravity == 1 and sm.localGravityController[self.interactable.id] then 
-		self.server_onFixedUpdate = sm.localGravityController[self.interactable.id] 
-		sm.localGravityController[self.interactable.id] = nil 
+	if dir == sm.vec3.new(0,0,1) and gravity == 1 and localGravityControllers[self.interactable.id] then 
+		self.server_onFixedUpdate = localGravityControllers[self.interactable.id] 
+		localGravityControllers[self.interactable.id] = nil 
 	return end -- kill the loop
-	if not sm.localGravityController[self.interactable.id] then sm.localGravityController[self.interactable.id] = self.server_onFixedUpdate end -- store the original
+	if not localGravityControllers[self.interactable.id] then localGravityControllers[self.interactable.id] = self.server_onFixedUpdate end -- store the original
     function self.server_onFixedUpdate(self, dt)
-		if not sm.localGravityController[self.interactable.id] then return end sm.localGravityController[self.interactable.id](self, dt) -- perform original
-		if not sm.exists(shape) then self.server_onFixedUpdate = sm.localGravityController[self.interactable.id] sm.localGravityController[self.interactable.id] = nil return end -- kill the loop
+		if not localGravityControllers[self.interactable.id] then return end localGravityControllers[self.interactable.id](self, dt) -- perform original
+		if not sm.exists(shape) then self.server_onFixedUpdate = localGravityControllers[self.interactable.id] localGravityControllers[self.interactable.id] = nil return end -- kill the loop
         for k, body in pairs(shape:getBody():getCreationBodies()) do -- do the gravity thing:
 			if sm.exists(body) then sm.physics.applyImpulse(body, (sm.vec3.new(0,0,1.047494) - dir * gravity)*sm.physics.getGravity()*dt*body.mass, true) end
         end
