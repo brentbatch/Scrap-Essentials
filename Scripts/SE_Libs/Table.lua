eae10f910590b4250c08f3edd4cc489eec37fca4
Brent <brent.batch@hotmail.com>
2019-07-25 17:44:29 +0200

    Revert "uppercase"
    This reverts commit a41d069df08888e0ad08a5894be5993454835de7.

--- Scripts/SE_Libs/Table.lua
+++
@@ -1,9 +0,0 @@
-local version = 1
-
-if (sm.__SE_Version._Table or 0) >= version then return end
-sm.__SE_Version._Table = version
-
-print("Loading extra table stuff")
-
--- get table size
-function table.size( someTable ) local i = 0 for k, v in pairs(someTable) do i = i + 1 end return i end
