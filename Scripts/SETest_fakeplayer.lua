228ab05772c2a3201b5a1990b7e0debbd4cc9c0a
BlueFlame-SM <timo.post@xs4all.nl>
2019-08-03 16:41:35 +0200

    updated fakeplayer
    - fixed examples
    - added examples to parts.json
    - added optional sort to getFakes

--- Scripts/SETest_fakeplayer.lua
+++ Scripts/SETest_fakeplayer.lua
@@ -4,7 +4,23 @@ dofile "SE_Loader.lua"
 -- the following code prevents re-load of this file, except if in '-dev' mode.  -- fixes broken sh*t by devs.
 if PlayerImitator and not sm.isDev then -- increases performance for non '-dev' users.
 	return -- perform sm.checkDev(shape) in server_onCreate to set sm.isDev
-end 
+end
+
+-- required to fix printing jank, plus it has a nice layout
+function tprint( tbl, indent )
+	if not tbl then return end
+	  
+	if not indent then indent = 0 end
+	for k, v in pairs(tbl) do
+		formatting = string.rep("  ", indent) .. k .. ": "
+		if type(v) == "table" then
+			print(formatting)
+			tprint(v, indent+1)
+			else
+			print(formatting .. tostring(v))
+		end
+	end
+end
 
 
 PlayerImitator = class()
@@ -17,6 +33,8 @@ PlayerImitator.colorHighlight = sm.color.new(0xef8010ff)
 
 
 function PlayerImitator.server_onCreate(self)
+	print(self.shape.worldPosition)
+	sm.isDev = sm.checkDev( self.shape )
 	sm.fakePlayer.createFake( self.shape ) -- puts self.shape into the fakePlayer list when spawned
 end
 
@@ -37,28 +55,24 @@ PlayerTracker.colorHighlight = sm.color.new(0xef8010ff)
 
 
 function PlayerTracker.server_onFixedUpdate(self)
-
 	local position = self.shape.worldPosition
-	local fakePlayers = {}								-- changes the table structure from containing id's to a simple array
-	for k,v in pairs(sm.fakePlayer.getFakes()) do		-- 
-		for k,v in pairs(sm.fakePlayer.getFakes()) do	-- 
-			table.insert(fakePlayers, v)				-- 
-		end												-- 
-	end													-- 
+	local fakePlayers = sm.fakePlayer.getFakes( )
 	
-	local distance = (position - fakePlayers[1]:getWorldPosition()):length() -- got distance using function getWorldPosition() instead of variable worldPosition because a faked position requires a function to be called
-	local trackedFakePlayer = fakePlayers[1]
-	
-	for k,v in pairs(fakePlayers) do
-		local fakeplayerDistance =  (position - v:getWorldPosition()):length()
-		if fakeplayerDistance < distance then
-			fakeplayerDistance = distance
-			trackedFakePlayer = v
+	if fakePlayers then
+		local distance = (position - fakePlayers[1]:getWorldPosition()):length() -- got distance using function getWorldPosition() instead of variable worldPosition because a faked position requires a function to be called
+		local trackedFakePlayer = fakePlayers[1]
+		
+		-- to get closest fakePlayer:
+		for k,v in pairs(fakePlayers) do
+			local fakeplayerDistance =  (position - v:getWorldPosition()):length()
+			if fakeplayerDistance < distance then
+				fakeplayerDistance = distance
+				trackedFakePlayer = v
+			end
 		end
+		
+		print(trackedFakePlayer:getWorldPosition()) -- or do whatever you want with this trackedFakePlayer
 	end
-	
-	print(trackedFakePlayer) -- or do whatever you want with this trackedFakePlayer
-	
 end
 
 
@@ -75,26 +89,11 @@ PlayerSpoofer.colorNormal = sm.color.new(0xdf7000ff)
 PlayerSpoofer.colorHighlight = sm.color.new(0xef8010ff)
 
 function PlayerSpoofer.server_onCreate(self)
+	print(self.shape.worldPosition + sm.vec3.new(0,0,1))
 	
 	local fakePlayer = {id = self.shape.id} -- can be local, can also be stored in self.fakePlayer
 	function fakePlayer.getWorldPosition(self_fakePlayer)
-		if self_fakePlayer.lastTick == sm.game.getCurrentTick() then
-			return self_fakePlayer.worldPosition
-		end
-		
-		local fakePosition = sm.vec3.new(0,0,0)
-		for k, v in pairs(self.interactable:getParents()) do
-			if tostring(v.shape.color) == "eeeeeeff" then
-				fakePosition.x = v.power
-			elseif tostring(v.shape.color) == "7a7a7aff" then
-				fakePosition.y = v.power
-			elseif tostring(v.shape.color) == "222222ff" then
-				fakePosition.z = v.power
-			end
-		end
-		self_fakePlayer.worldPosition = fakePosition
-		
-		return self_fakePlayer.worldPosition
+		return self.shape.worldPosition + sm.vec3.new(0,0,1)
 	end
 	-- the function is created here, so it has acces to any variables defined in the script (or at least same location, so not ones defined after it or in a different scope).
 	-- in this case, if you call the function from a different block through the fakePlayer, even though the function is called by a different block, the 'self' called will be from the block that made the function.
